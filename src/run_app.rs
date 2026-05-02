916987ee2cbdfe6b40d783a9ea0f9c4b18a15050
Aritra Sanyal <aritras2003@gmail.com>
2026-05-02 13:49:12 +0530

    mod: removed comments

--- src/run_app.rs
+++ src/run_app.rs
@@ -1,17 +1,14 @@
 use crate::ui;
 use crossterm::event::{self, Event, KeyCode};
-use ratatui::{Terminal, backend::Backend};
+use ratatui::{backend::Backend, Terminal};
 
 pub fn run<B: Backend>(terminal: &mut Terminal<B>) -> Result<(), Box<dyn std::error::Error>>
 where
     B::Error: 'static,
 {
     loop {
-        // This is where the magic happens:
-        // It passes the 'Frame' from the terminal to your ui.rs function
         terminal.draw(|f| ui::render(f))?;
 
-        // Basic event handling to let you exit
         if let Event::Key(key) = event::read()? {
             if let KeyCode::Char('q') = key.code {
                 return Ok(());
